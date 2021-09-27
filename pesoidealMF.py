820947efbff453cd18010dcb3375c1d01b12fafb
Esdras_Silva <54448344+SilvaEsdras@users.noreply.github.com>
2021-09-27 20:28:37 -0300

    Update pesoidealMF.py

--- pesoidealMF.py
+++ pesoidealMF.py
@@ -1,12 +1,12 @@
-print('Vamos calcular o seu peso ideal? ')
+print('Vamos calcular o seu peso ideal? \n')
 
-sexo = input('Digite M para masculino \n ou Digite F para Feminino \n')
+sexo = input('Digite 1 para masculino \n ou Digite 2 para Feminino \n')
 altura = float(input('Qual é sua altura? '))
 
-if (sexo == "m", "M"):
+if (sexo == '1'):
   print('Seu peso ideal é', (72.7*altura)-58)
 
-elif (sexo == "f", "F"):
+elif (sexo == '2'):
   print('Seu peso ideal é', (62.1*altura)-44.7)
 
 else:
